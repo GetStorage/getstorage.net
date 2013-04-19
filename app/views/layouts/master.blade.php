977ff1f95ab6f086eccac5f7cd086eb7b8dbb3e4
Luke Strickland <luke@axxim.net>
2013-04-19 12:30:34 -0500

    Added some contact us stuff to the bottom

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -124,38 +124,31 @@
 <!-- FOOTER -->
 <footer id="footer">
     <div class="container">
-        <?php /*<div class="row">
+        <div class="row">
             <div class="span3 col">
                 <div class="block contact-block">
                     <!--@todo: replace with company contact details-->
                     <h3>Contact Us</h3>
                     <address>
-                        <p><abbr title="Phone"><i class="icon-phone"></i></abbr> 019223 8092344</p>
-                        <p><abbr title="Email"><i class="icon-envelope"></i></abbr> info@appstrap.me</p>
-                        <p><abbr title="Address"><i class="icon-home"></i></abbr> Sunshine House, Sunville. SUN12 8LU.</p>
+                        <p><abbr title="IRC"><i class="icon-group"></i></abbr> irc.esper.net #storage</p>
+                        <p><abbr title="Email"><i class="icon-envelope"></i></abbr> help@getstorage.net</p>
                     </address>
                 </div>
             </div>
             <div class="span5 col">
                 <div class="block">
                     <h3>About Us</h3>
-                    <p>Making the web a prettier place one template at a time! We make beautiful, quality, responsive Drupal & web templates!</p>
+                    <p>Storage is an awesome mixture of file hosting, and a filesystem for the web. </p>
                 </div>
             </div>
             <div class="span4 col">
                 <div class="block newsletter">
-                    <h3>Newsletter</h3>
-                    <p>Stay up to date with our latest news and product releases by signing up to our newsletter.</p>
-                    <!--@todo: replace with mailchimp code-->
-                    <form class="form-inline">
-                        <div class="input-append">
-                            <input class="input-medium" type="text" placeholder="Email" />
-                            <button class="btn btn-primary" type="button">Go!</button>
-                        </div>
-                    </form>
+                    <h3>Placeholder</h3>
+                    <p>Something will go here in the future.</p>
+
                 </div>
             </div>
-        </div>*/ ?>
+        </div>
         <div class="row-fluid">
             <div id="toplink"><a href="#top" class="top-link" title="Back to top">Back To Top <i class="icon-chevron-up"></i></a></div>
             <!--@todo: replace with company copyright details-->
