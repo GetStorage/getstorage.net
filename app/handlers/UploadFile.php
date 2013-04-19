ce1f1ecba7ecc5de6c92b0445f86080ce40ccb10
Luke Strickland <luke@axxim.net>
2013-04-19 15:46:20 -0500

    Allow S3 to properly serve to the cache servers

--- app/handlers/UploadFile.php
+++ app/handlers/UploadFile.php
@@ -16,6 +16,7 @@ class UploadFile {
             'Bucket' => 'storag',
             'Key' => $object->name,
             'SourceFile' => $path,
+            'ContentType' => $object->mime,
         ));
 
         $object->s3 = true;
