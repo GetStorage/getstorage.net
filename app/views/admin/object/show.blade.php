d4df713486f3bc4a47892902aa4e837122cc1195
Luke Strickland <luke@axxim.net>
2013-04-25 16:32:47 -0500

    I forgot this poor view :(

--- app/views/admin/object/show.blade.php
+++ app/views/admin/object/show.blade.php
@@ -6,25 +6,30 @@ Admin - Object
 
 @section('content')
 
-<h2>{{$object->name}}</h2>
+<div class="row">
+    @include('admin.partial.navigation')
+    <div class="span9">
+        <h2>{{$object->name}}</h2>
 
-<table class="table">
-    <thead>
-        <tr>
-            <th>IP</th>
-            <th>Referrer</th>
-            <th>Date</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($analytics as $stat)
-        <tr>
-            <td>{{$stat->ip}}</td>
-            <td>{{@json_decode($stat->data, true)['HTTP_REFERER']}}</td>
-            <td>{{$stat->created_at}}</td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+        <table class="table">
+            <thead>
+            <tr>
+                <th>IP</th>
+                <th>Referrer</th>
+                <th>Date</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($analytics as $stat)
+            <tr>
+                <td>{{$stat->ip}}</td>
+                <td>{{@json_decode($stat->data, true)['HTTP_REFERER']}}</td>
+                <td>{{$stat->created_at}}</td>
+            </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 
 @stop
