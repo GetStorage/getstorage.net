d0e35d4efdc0c0355d94c0b968907ed3eef65d7b
Kamal Nasser <kamal@kamalnasser.net>
2013-05-07 19:21:30 +0300

    ApiBaseController

---
+++ app/controllers/api/v2/ApiBaseController.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace ApiVersionTwo;
+
+
+class ApiBaseController extends \BaseController {
+
+    private $user;
+
+    public function __construct() {
+        $requestedKey = (\Request::header('Storage-Key') != false ? \Request::header('Storage-Key') : \Input::get('key'));
+        $key = \Key::where('key', $requestedKey)->first();
+        $this->user = \User::find($key->user_id);
+    }
+
+}
--- app/controllers/api/v2/ApiCfsController.php
+++ app/controllers/api/v2/ApiCfsController.php
@@ -4,15 +4,7 @@
 namespace ApiVersionTwo;
 
 
-class ApiCfsController extends \BaseController {
-
-    private $user;
-
-    public function __construct() {
-        $requestedKey = (\Request::header('Storage-Key') != false ? \Request::header('Storage-Key') : \Input::get('key'));
-        $key = \Key::where('key', $requestedKey)->first();
-        $this->user = \User::find($key->user_id);
-    }
+class ApiCfsController extends BaseController {
 
     /**
      * Get all objects from the user
